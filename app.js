c38458b8b0444ff5f56d4da746f27e06e8dfedab
dead-horse <dead_horse@qq.com>
2018-03-25 00:54:48 +0800

    fix: deprecate ignoreJSON

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const safeRedirect = require('./lib/safe_redirect');
 module.exports = app => {
   app.config.coreMiddleware.push('securities');
 
+  if (app.config.security.csrf && app.config.security.csrf.ignoreJSON) {
+    app.deprecate('[egg-security] `app.config.security.ignoreJSON` is not safe now, please disable it.');
+  }
+
   // patch response.redirect
   safeRedirect(app);
 };
