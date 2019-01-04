0c6634cc07791f483760c2ebc1013e4d3fb6d24c
guoshencheng <648772021@qq.com>
2019-01-04 15:33:39 +0800

    test: add test for Referrer-Policy is set to index of item in ALLOWED_POLICIES_ENUM

--- test/referrer.test.js
+++ test/referrer.test.js
@@ -46,5 +46,15 @@ describe('test/referrer.test.js', function() {
         .expect(500, done);
     });
 
+    // check for fix https://github.com/eggjs/egg-security/pull/50
+    it('should throw error when Referrer-Policy is set to index of item in ALLOWED_POLICIES_ENUM', function(done) {
+      const policy = 0;
+      this.app2.httpRequest()
+        .get(`/referrer?policy=${policy}`)
+        .set('accept', 'text/html')
+        .expect(new RegExp(`"${policy}" is not available.`))
+        .expect(500, done);
+    });
+
   });
 });
